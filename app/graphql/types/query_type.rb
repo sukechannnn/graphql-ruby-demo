730aab86888b8c07817ff05307fdffed47e6b998
kielze <wonderfulworld0315@gmail.com>
2017-12-14 23:05:42 +0900

    Implement Types::UserType & user field of Query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,9 @@
 Types::QueryType = GraphQL::ObjectType.define do
   name 'Query'
-  # Add root-level fields here.
-  # They will be entry points for queries on your schema.
 
-  # TODO: remove me
-  field :testField, types.String do
-    description 'An example field added by the generator'
-    resolve ->(_obj, _args, _ctx) {
-      'Hello World!'
+  field :user, !Types::UserType do
+    resolve ->(_obj, _args, ctx) {
+      ctx[:current_user]
     }
   end
 end
