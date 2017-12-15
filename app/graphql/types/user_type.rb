9dd5847a043d257f358f766851fa0abe0c3eb805
kielze <wonderfulworld0315@gmail.com>
2017-12-15 14:13:36 +0900

    Add description

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,9 +1,9 @@
 Types::UserType = GraphQL::ObjectType.define do
   name 'User'
 
-  field :id, !types.ID
-  field :name, !types.String
-  field :email, !types.String
-  field :address, !Types::AddressType
-  connection :posts, !Types::PostType.connection_type
+  field :id, !types.ID, 'ユニークな ID'
+  field :name, !types.String, '名前'
+  field :email, !types.String, 'e-mail アドレス'
+  field :address, !Types::AddressType, '住所'
+  connection :posts, !Types::PostType.connection_type, '投稿一覧'
 end
