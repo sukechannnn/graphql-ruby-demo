3a2fec22ff6d8f840a8bb7647cb9e87a6e13bd57
kielze <wonderfulworld0315@gmail.com>
2017-12-15 11:17:39 +0900

    Implement UpdateAddress mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,11 +1,5 @@
 Types::MutationType = GraphQL::ObjectType.define do
   name 'Mutation'
 
-  # TODO: Remove me
-  field :testField, types.String do
-    description 'An example field added by the generator'
-    resolve ->(_obj, _args, _ctx) {
-      'Hello World!'
-    }
-  end
+  field :updateAddress, field: Mutations::UpdateAddressMutation.field
 end
