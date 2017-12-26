55782fc20b0d72c772c2b4a2aaa9761d4bc70cb5
kielze <wonderfulworld0315@gmail.com>
2017-12-26 12:16:49 +0900

    Move test for graphql schema

--- spec/graphql/graphql_ruby_demo_schema_spec.rb
+++ spec/graphql/graphql_ruby_demo_schema_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'GraphqlRubyDemoSchema' do
   let(:current_definition) { GraphqlRubyDemoSchema.to_definition }
-  let(:printout_definition) { File.read(Rails.root.join('schema.graphql')) }
+  let(:printout_definition) { File.read(Rails.root.join('spec', 'graphql', 'schema.graphql')) }
 
   it 'equals dumped schema, `rake graphql:schema:dump` please!' do
     expect(current_definition).to eq(printout_definition)
